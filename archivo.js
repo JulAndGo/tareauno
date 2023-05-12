2797c483d9d274de25ceb32643ec3e616cd5667b
Juliana Gomez <juliana.andrea.gomez@correounivalle.edu.co>
2023-05-12 14:36:04 -0500

    se agregan cambios a la rama

--- archivo.js
+++ archivo.js
@@ -1,5 +1,3 @@
 console.log ("Juliana Andrea Gómez Peláez");
 console.log ("2226420-3744");
 console.log ("Metodologías de Desarrollo de Software");
-console.log ("Metodologías de Desarrollo de Software");
-
